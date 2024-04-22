2c0ac042edbcf681eaf063325134e1e881143195
Ali <ali2.suhail@live.uwe.ac.uk>
2024-04-22 17:25:30 +0100

    Changed file structure, Finished Implementation and Evaluation Chapters, reagganged test files for Application and Design

--- AFNT/Application/tests/map.py
+++
@@ -1,9 +0,0 @@
-from kivymd.app import MDApp
-from kivy_garden.mapview import MapView
-
-class MapViewApp(MDApp):
-    def build(self):
-        mapview = MapView(zoom=10, lat=51.454514, lon=-2.587910)
-        return mapview
-    
-MapViewApp().run()
--- AFNT/ArduinoWatch/ArduinoWatch/stepCounter.ino
+++ AFNT/ArduinoWatch/ArduinoWatch/stepCounter.ino
@@ -11,7 +11,6 @@ unsigned long lastSample = 0;
 int sampleNew = 0;
 int sampleOld = 0;
 int precision = 10;
-int stepsInLastThirtyMinutes = 0; // used to keep track of recent steps to notify wearer of inactive periods
 bool halfStep = false;
 bool stepAlert = false;
 
@@ -19,33 +18,35 @@ bool stepAlert = false;
 
 int totalSteps = 0;
 
-int isValidStepPattern(){
+int isValidStepPattern() {
   return validStepPattern;
 }
 
-int getTotalSteps(){
+int getTotalSteps() {
   return totalSteps;
 }
 
-int getStepTrigger()
-{
+int getStepTrigger() {
   return STEP_TRIGGER;
 }
 
 int updatePedometer() {
- // if (millis() < sampleInterval + lastSample)return 0; we don't need to check this as it takes longer than sampleInterval to updatePulse anyway
-
+  // Record the current time
   lastSample = millis();
 
-  accel_sensor.read();//This function gets new data from the acccelerometer
+  // Read new data from the accelerometer
+  accel_sensor.read();
 
+  // Calculate the magnitude (A) of the acceleration vector
   int sum = pow(accel_sensor.X, 2);
   sum += pow(accel_sensor.Y, 2);
   sum += pow(accel_sensor.Z, 2);
   int A = sqrt(sum);
-  
+
+  // Compute the difference between the current and previous acceleration magnitudes
   int difference = abs(A - sampleOld);
 
+  // Apply filtering to smooth out small changes
   if (difference < 10) {
     difference = 0;
   }
@@ -53,34 +54,38 @@ int updatePedometer() {
     difference = 30;
   }
 
+  // Determine the sign of the change in acceleration magnitude
   if (A - sampleOld > 0) {
     difference = sampleOld + difference;
   } else {
     difference = sampleOld - difference;
   }
 
-
+  // Store the difference in the acceleration buffer
   aBuff[aBuffPos] = difference;
 
+  // Find the minimum and maximum values in the acceleration buffer
   int Amin = 1000;
   int Amax = 0;
   for (int i = 0; i < amtSamples; i++) {
     int sampleVal = aBuff[i];
-    if (sampleVal < Amin)Amin = sampleVal;
-    if (sampleVal > Amax)Amax = sampleVal;
+    if (sampleVal < Amin) Amin = sampleVal;
+    if (sampleVal > Amax) Amax = sampleVal;
   }
 
+  // Calculate the peak-to-peak value and set a threshold
   int peakToPeak = Amax - Amin;
   int threshold = (peakToPeak / 2) + Amin;
 
+  // Update the previous sample value
   sampleOld = sampleNew;
 
+  // Check for a potential new step based on acceleration changes
   bool newStep = false;
   if (abs(sampleNew - difference) > precision) {
     sampleNew = difference;
     if (peakToPeak > 70 && sampleOld > threshold && sampleNew < threshold) {
-      //Serial.println("Step!");
-      //potential step signal
+      // Check if the step falls within a valid time interval
       if (validStepPattern) {
         if (millis() > lastStepTime + stepIntervalLow && millis() < lastStepTime + stepIntervalHigh) {
           newStep = true;
@@ -91,55 +96,43 @@ int updatePedometer() {
         newStep = true;
         validStepPattern = true;
       }
+      // Update the last step time and increment the step count
       lastStepTime = millis();
       if (newStep) {
-        if(false)
-        {
+        if (false) {
           stepAlert = true;
         }
         totalSteps++;
+        // Display the total steps on the serial monitor
         SerialUSB.print("totalSteps:  ");
         SerialUSB.println(totalSteps);
-        stepTimestamps[firstMinusOne()] = millis(); // adds the most recent step to the array containing the timestamps of the most recent steps
+        // Add the timestamp of the most recent step to the timestamps array
+        stepTimestamps[firstMinusOne()] = millis();
       }
     }
   }
 
+  // Update the buffer position and reset if necessary
   aBuffPos++;
   if (aBuffPos >= amtSamples) {
     aBuffPos = 0;
   }
-  if (newStep /*&& (totalSteps & 1) == 0*/)
+
+  // Return 1 if a new step is detected, otherwise return 0
+  if (newStep /*&& (totalSteps & 1) == 0*/) {
     return 1;
+  }
   return 0;
 }
 
-void clearStepCount()
-{
-  totalSteps=0;
-  resetStepCounters();
-}
-
-
-void dailyStepReset()
-{
-  if(rtc.getHours() == 0 && rtc.getMinutes() == 0)
-  {
-    clearStepCount();
-  }
-}
 
 int firstMinusOne() // returns the index in the stepTimestamps array that the next recorded step timestamp will be stored in
 {
-  for(int i = 0; i<getStepTrigger(); ++i)
-  {
-    if(stepTimestamps[i] == 4294967295)
-    {
-      //SerialUSB.print("First branch: ");
-      //SerialUSB.println(i);
-      return i; // if all array values have not been previously used, then we want to place the most recent value in the first position containing 4294967295
-    }
-       else if(i == getStepTrigger()-1) // if all array values have been previously used, then we want to shift array to the left and add the most recent value in the final position
+  for (int i = 0; i < getStepTrigger(); ++i) {
+    // If all array values have not been previously used, then we want to place the most recent value in the first position containing 4294967295
+    if (stepTimestamps[i] == 4294967295) {
+      return i;
+    } else if (i == getStepTrigger() - 1) // if all array values have been previously used, then we want to shift array to the left and add the most recent value in the final position
     {
       //SerialUSB.print("Second branch: ");
       //SerialUSB.println(i);
@@ -149,39 +142,31 @@ int firstMinusOne() // returns the index in the stepTimestamps array that the ne
   }
 }
 
-void initStepTimestamps()
-{
-  for(int i = 0; i<getStepTrigger(); ++i)
-  {
-    stepTimestamps[i]= 4294967295;
+void initStepTimestamps() {
+  for (int i = 0; i < getStepTrigger(); ++i) {
+    stepTimestamps[i] = 4294967295;
   }
 }
 
-void shiftLeft()
-{
-  for(int i = 1; i<getStepTrigger(); ++i)
-  {
-    stepTimestamps[i-1] = stepTimestamps[i];
+void shiftLeft() {
+  for (int i = 1; i < getStepTrigger(); ++i) {
+    stepTimestamps[i - 1] = stepTimestamps[i];
   }
-  stepTimestamps[getStepTrigger()-1] = 4294967295;
+  stepTimestamps[getStepTrigger() - 1] = 4294967295;
 }
 
-void serialStepTimestamps()
-{
-  for(int i = 0; i<250; ++i)
-  {
+void serialStepTimestamps() {
+  for (int i = 0; i < 250; ++i) {
     SerialUSB.print(i);
     SerialUSB.print(": ");
     SerialUSB.println(stepTimestamps[i]);
   }
 }
 
-bool getStepAlert()
-{
+bool getStepAlert() {
   return stepAlert;
 }
 
-void resetStepAlert()
-{
+void resetStepAlert() {
   stepAlert = false;
 }
